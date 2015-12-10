3e3447b2e9a868bcde7fa54fa8f3a4f3899dbffa
alu0100830064 <alu0100830064@ull.edu.es>
2015-12-10 19:04:31 +0000

    Añadiendo metodo to_s

--- lib/prct08/doble.rb
+++ lib/prct08/doble.rb
@@ -152,7 +152,7 @@ class Cambio < Clase_Madre
 end
     
     
-end
+
     
 
     
@@ -202,7 +202,50 @@ class Libro < Cambio
     def getisbn
         return @isbn
     end
-end 
+    
+def to_s
+            
+    
+        puts ""
+        for i in 0..@autor.size-1
+            
+            if i == @autor.size-2
+                print ("#{@autor[i]}  &  ")
+            else
+                print ("#{@autor[i]}, ")
+                
+            end
+        end
+        
+            puts""
+            print "\t"
+            puts ("#{@titulo}")
+            
+            print "\t"
+            puts ("#{@serie}")
+        
+            print "\t"
+            puts ("#{@editorial}")
+        
+            print "\t"
+            puts ("#{@edicion}")
+        
+            print "\t"
+            puts ("#{@fecha}")
+            
+            
+            
+            @isbn.each { |i, valor|
+            print "\t"
+            puts ("#{i}  #{valor}") } 
+            
+ 
+        end 
+    
+
+
+end
+
 
 
 
